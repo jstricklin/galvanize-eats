3e9868097bb27b605799ded327acbb5149c936be
Jose Stricklin <josestricklin@El-Diablo.local>
2018-10-09 15:33:10 -0600

    complete basic DB

--- reads-angular/src/app/app-routing/app-routing.module.ts
+++ reads-angular/src/app/app-routing/app-routing.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from '../dashboard/dashboard.component';
+import { BookListComponent } from '../book-list/book-list.component';
+import { AuthorListComponent } from '../author-list/author-list.component';
 
 const routes: Routes = [{
     path: '',
     component: DashboardComponent,
-}];
+},
+    {
+        path: 'books',
+        component: BookListComponent,
+    },
+    {
+        path: 'authors',
+        component: AuthorListComponent,
+    }
+];
 
 @NgModule({
     imports: [
--- server/knexfile.js
+++ server/knexfile.js
@@ -2,43 +2,13 @@
 
 module.exports = {
 
-  development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3'
-    }
-  },
+    development: {
+        client: 'pg',
+        connection: 'postgress://localhost/galvanize_reads'
+    },
 
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
+    production: {
+        client: 'pg',
+        connection: process.env.DATABASE_URL,
     }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
 };
