68b4c34a8e7dedc917912696407bea16cb0a1e1c
Jose Stricklin <josestricklin@El-Diablo.local>
2018-10-15 01:49:37 -0600

    add author portrait

--- reads-react/src/components/Authors.js
+++ reads-react/src/components/Authors.js
@@ -7,18 +7,8 @@ class Authors extends Component {
             authorData: [],
         }
     }
-    authorCard(author) {
-        return (
-            <div className="card">
-                <div className="card-title">
-                    <h5>{author.first} {author.last}</h5>
-                </div>
-            </div>
-            )
-    }
-
     renderAuthors() {
-        return this.state.authorData.map(author => <React.Fragment>{this.authorCard(author)}</React.Fragment>)
+        return this.state.authorData.map(author => <React.Fragment><AuthorCard author={author} /></React.Fragment>)
     }
     componentDidMount() {
         this.props.getAuthors()
@@ -32,11 +22,38 @@ class Authors extends Component {
                 <h1> Authors </h1>
                 <div className="bg-warning py-3">
                     <div className="container d-flex flex-wrap justify-content-around">
-                            {this.renderAuthors()}
+                        {this.renderAuthors()}
                     </div>
                 </div>
             </div>
             )
     }
 }
+class AuthorCard extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            hide: true,
+
+        }
+    }
+    render() {
+        return (
+            <div>
+            <div className="card" onClick={()=>this.setState({hide: !this.state.hide})}>
+                <div className="card-title">
+                    <h5>{this.props.author.first} {this.props.author.last}</h5>
+                </div>
+                <div className="card-image">
+                    <img src={this.props.author.portraitURL} alt="author pic" />
+                </div>
+                {this.state.hide ? null : <div className="card-body">
+                    <p>{this.props.author.bio}</p>
+                </div> }
+            </div>
+            </div>
+            )
+    }
+}
+
 export default Authors;
